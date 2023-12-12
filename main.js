5aed8796769fd089d32253bf348f19bb8f752356
JORDANWOODS1234 <151679739+JORDANWOODS1234@users.noreply.github.com>
2023-12-12 11:51:23 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,180 +1,163 @@
-const numb = document.querySelector('#number');
-const startScreen = document.querySelector('.startScreen');
-const gameMsg = document.querySelector('.gameMsg');
-const gameArea = document.querySelector('.gameArea');
-
-let keys = {
+// Background scrolling speed 
+let move_speed = 3; 
     
-};
-
-let player = {
-    speed: 3,
-    score: 0
-};
-
-
-gameMsg.addEventListener('click',start);
-startScreen.addEventListener('click',start);
-
-document.addEventListener('keydown',pressOn);
-document.addEventListener('keyup',pressOff);
-
-function start(){
-    gameMsg.classList.add('hide');
-    startScreen.classList.add('hide');
-    gameArea.innerHTML = "";
-    player.inplay = true;
-    player.score = 0;
-    let bird = document.createElement('div');
-    bird.setAttribute('class','bird');
-    let eye = document.createElement('span');
-    eye.className = 'eye';
-    bird.appendChild(eye);
-    let wing = document.createElement('span');
-    wing.className = 'wing';
-    bird.appendChild(wing);
-    wing.pos = 16;
-    wing.style.top = wing.pos + 'px';
-    gameArea.appendChild(bird);
-    player.x= bird.offsetLeft;
-    player.y = bird.offsetTop;
-
-    player.pipe = 0;
-    let spacing = 550;
-    let howMany = Math.floor( (gameArea.offsetWidth)/spacing);
-        // console.log(howMany);
-    for(let i=0;i<howMany;++i){
-        buildPipes(player.pipe * spacing);
-    }
-    window.requestAnimationFrame(playGame);
-}
-
-function movePipes(bird){
-    let lines = document.querySelectorAll('.pipe');
-    let cnt = 0;
-    lines.forEach( item => {
-        // console.log(item);
-        item.x -= player.speed;
-        item.style.left = item.x + 'px';
-        if(item.x < 0){
-            item.parentElement.removeChild(item);
-            cnt++;
-        }
-        if(isCollide(item,bird)){
-            playGameOver(bird);
-        }
-    });
-    cnt /= 2;
-    for(let i=0;i<cnt;++i){
-        buildPipes(0);
-    }
-}
-
-function isCollide(a,b){
-    let aRect = a.getBoundingClientRect();
-    let bRect = b.getBoundingClientRect();
-    return !(
-        (aRect.bottom < bRect.top) ||
-        (aRect.top > bRect.bottom) ||
-        (aRect.right < bRect.left) ||
-        (aRect.left > bRect.right)
-    )
-}
-
-function playGame() {
-    if(player.inplay){
-        let bird = document.querySelector('.bird');
-        let wing = document.querySelector('.wing');
-        movePipes(bird);
-        let move = false;
-        if(keys.ArrowLeft && player.x>30){
-            player.x -= player.speed;
-            move = true;
-        }
-        if(keys.ArrowRight && player.x < gameArea.offsetWidth-60){  
-        player.x += player.speed;
-        move = true;
-        }
-        if( (keys.ArrowUp || keys.Space)&&player.y>80){
-            player.y -= (player.speed*5);
-            move = true;
-        }
-        if(keys.ArrowDown && player.y < 880){
-            player.y += player.speed;
-            move = true;
-        }
-
-        if(move) {
-            wing.pos = wing.pos == 16 ? 25:16;
-            wing.style.top = wing.pos + 'px';
-        }
-
-        if(player.y < 1000)  player.y += (player.speed*2)
-        if(player.y > 910){
-            playGameOver(bird);
-        }
+// Gravity constant value 
+let gravity = 0.5; 
+    
+// Getting reference to the bird element 
+let bird = document.querySelector('.bird'); 
+    
+// Getting bird element properties 
+let bird_props = bird.getBoundingClientRect(); 
+let background = 
+    document.querySelector('.background') 
+            .getBoundingClientRect(); 
+    
+// Getting reference to the score element 
+let score_val = 
+    document.querySelector('.score_val'); 
+let message = 
+    document.querySelector('.message'); 
+let score_title = 
+    document.querySelector('.score_title'); 
+    
+// Setting initial game state to start 
+let game_state = 'Start'; 
+    
+// Add an eventlistener for key presses 
+document.addEventListener('keydown', (e) => { 
+    
+  // Start the game if enter key is pressed 
+  if (e.key == 'Enter' && 
+      game_state != 'Play') { 
+    document.querySelectorAll('.pipe_sprite') 
+              .forEach((e) => { 
+      e.remove(); 
+    }); 
+    bird.style.top = '40vh'; 
+    game_state = 'Play'; 
+    message.innerHTML = ''; 
+    score_title.innerHTML = 'Score : '; 
+    score_val.innerHTML = '0'; 
+    play(); 
+  } 
+}); 
+function play() { 
+  function move() { 
+      
+    // Detect if game has ended 
+    if (game_state != 'Play') return; 
+      
+    // Getting reference to all the pipe elements 
+    let pipe_sprite = document.querySelectorAll('.pipe_sprite'); 
+    pipe_sprite.forEach((element) => { 
         
-        bird.style.left = player.x + 'px';
-        bird.style.top = player.y + 'px';
- 
-        player.score += 0.5;
-        numb.innerHTML = Math.floor(player.score);
-        if(!(player.score%1000))  player.speed+=0.5;
-        window.requestAnimationFrame(playGame);
-    }
-}
-
-function playGameOver(bird){
-    player.inplay=false;
-    player.speed = 3;
-    gameMsg.classList.remove('hide');
-    bird.setAttribute('style','transform:rotate(180deg)');
-    gameMsg.innerHTML = "GameOver. Click to here start again";
-}
-
-function buildPipes(startPos){
-    let totalH = gameArea.offsetHeight;
-    let totalW = gameArea.offsetWidth;
-    player.pipe++;
-    let clr = randamColor();
-    let newPipe1 = document.createElement('div');
-    newPipe1.start = startPos + totalW;
-    newPipe1.classList.add('pipe');
-    newPipe1.innerHTML = player.pipe;
-    newPipe1.height = Math.floor(Math.random()*350);
-    newPipe1.style.height = newPipe1.height + 'px';
-    newPipe1.style.left = newPipe1.start + 'px';
-    newPipe1.style.top = '8px';
-    newPipe1.x = newPipe1.start;
-    newPipe1.id = player.pipe;
-    newPipe1.style.backgroundColor = clr;
-    gameArea.appendChild(newPipe1);
-
-    let pipeSpace = Math.floor(Math.random()*250) + 180;
-    let newPipe2 = document.createElement('div');
-    newPipe2.start = newPipe1.start;
-    newPipe2.classList.add('pipe');
-    newPipe2.innerHTML = player.pipe;
-    newPipe2.style.height = totalH-newPipe1.height-pipeSpace + 'px';
-    newPipe2.style.left = newPipe1.start + 'px';
-    newPipe2.style.bottom = '12px';
-    newPipe2.x = newPipe1.start;
-    newPipe2.id = player.pipe;
-    newPipe2.style.backgroundColor = clr;
-    gameArea.appendChild(newPipe2);
-
-}
-
-function randamColor(){
-    return '#'+Math.random().toString(16).substr(-6);
-}
-
-function pressOn(e){
-    e.preventDefault();
-    keys[e.code] = true;
-}
-
-function pressOff(e) {
-    e.preventDefault();
-    keys[e.code] = false;
-}
+      let pipe_sprite_props = element.getBoundingClientRect(); 
+      bird_props = bird.getBoundingClientRect(); 
+        
+      // Delete the pipes if they have moved out 
+      // of the screen hence saving memory 
+      if (pipe_sprite_props.right <= 0) { 
+        element.remove(); 
+      } else { 
+        // Collision detection with bird and pipes 
+        if ( 
+          bird_props.left < pipe_sprite_props.left + 
+          pipe_sprite_props.width && 
+          bird_props.left + 
+          bird_props.width > pipe_sprite_props.left && 
+          bird_props.top < pipe_sprite_props.top + 
+          pipe_sprite_props.height && 
+          bird_props.top + 
+          bird_props.height > pipe_sprite_props.top 
+        ) { 
+            
+          // Change game state and end the game 
+          // if collision occurs 
+          game_state = 'End'; 
+          message.innerHTML = 'Press Enter To Restart'; 
+          message.style.left = '28vw'; 
+          return; 
+        } else { 
+          // Increase the score if player 
+          // has the successfully dodged the  
+          if ( 
+            pipe_sprite_props.right < bird_props.left && 
+            pipe_sprite_props.right +  
+            move_speed >= bird_props.left && 
+            element.increase_score == '1'
+          ) { 
+            score_val.innerHTML = +score_val.innerHTML + 1; 
+          } 
+          element.style.left =  
+            pipe_sprite_props.left - move_speed + 'px'; 
+        } 
+      } 
+    }); 
+  
+    requestAnimationFrame(move); 
+  } 
+  requestAnimationFrame(move); 
+  
+  let bird_dy = 0; 
+  function apply_gravity() { 
+    if (game_state != 'Play') return; 
+    bird_dy = bird_dy + gravity; 
+    document.addEventListener('keydown', (e) => { 
+      if (e.key == 'ArrowUp' || e.key == ' ') { 
+        bird_dy = -7.6; 
+      } 
+    }); 
+  
+    // Collision detection with bird and 
+    // window top and bottom 
+  
+    if (bird_props.top <= 0 || 
+        bird_props.bottom >= background.bottom) { 
+      game_state = 'End'; 
+      message.innerHTML = 'Press Enter To Restart'; 
+      message.style.left = '28vw'; 
+      return; 
+    } 
+    bird.style.top = bird_props.top + bird_dy + 'px'; 
+    bird_props = bird.getBoundingClientRect(); 
+    requestAnimationFrame(apply_gravity); 
+  } 
+  requestAnimationFrame(apply_gravity); 
+  
+  let pipe_seperation = 0; 
+    
+  // Constant value for the gap between two pipes 
+  let pipe_gap = 35; 
+  function create_pipe() { 
+    if (game_state != 'Play') return; 
+      
+    // Create another set of pipes 
+    // if distance between two pipe has exceeded 
+    // a predefined value 
+    if (pipe_seperation > 115) { 
+      pipe_seperation = 0 
+        
+      // Calculate random position of pipes on y axis 
+      let pipe_posi = Math.floor(Math.random() * 43) + 8; 
+      let pipe_sprite_inv = document.createElement('div'); 
+      pipe_sprite_inv.className = 'pipe_sprite'; 
+      pipe_sprite_inv.style.top = pipe_posi - 70 + 'vh'; 
+      pipe_sprite_inv.style.left = '100vw'; 
+        
+      // Append the created pipe element in DOM 
+      document.body.appendChild(pipe_sprite_inv); 
+      let pipe_sprite = document.createElement('div'); 
+      pipe_sprite.className = 'pipe_sprite'; 
+      pipe_sprite.style.top = pipe_posi + pipe_gap + 'vh'; 
+      pipe_sprite.style.left = '100vw'; 
+      pipe_sprite.increase_score = '1'; 
+        
+      // Append the created pipe element in DOM 
+      document.body.appendChild(pipe_sprite); 
+    } 
+    pipe_seperation++; 
+    requestAnimationFrame(create_pipe); 
+  } 
+  requestAnimationFrame(create_pipe); 
+} 
